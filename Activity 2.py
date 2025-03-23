cebf3d7493a135842211ade4ad2b749cbca24bc2
Avanthikacoding <aviedayilliam@gmail.com>
2025-03-23 12:28:12 +0000

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,7 @@
+variable = 10
+while variable > 0:
+    variable = variable - 1
+    if variable == 5:
+        continue 
+    print("\n You're current variable is:" , variable)
+print("\n Bye bye! For now anyway!")
